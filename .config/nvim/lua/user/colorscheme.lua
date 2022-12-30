0a8fdd0d8a7a0ff02bde96e2b89af24f1c5fecf4
Daniel Eric Rieder <dan.rieder@protonmail.com>
2022-12-30 14:04:08 -0800

    added comments for colorscheme variations

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,10 @@
-local colorscheme = "tokyonight"
+-- Colorscheme from github.com/folke/tokyonight.nvim
+-- local colorscheme = "tokyonight-night"
+-- local colorscheme = "tokyonight-storm"
+-- local colorscheme = "tokyonight-day"
+-- local colorscheme = "tokyonight-moon"
+
+local colorscheme = "tokyonight-night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
